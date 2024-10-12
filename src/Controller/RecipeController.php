b5114e57d72bac6d7fd223d04f956ff25cdb1692
jerome <jeromesauzet@yahoo.fr>
2024-10-12 11:04:47 +0200

    add show pages on front

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -70,7 +70,7 @@ class RecipeController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $recipe = $form->getData();
-
+            
             $manager->persist($recipe);
             $manager->flush();
 
